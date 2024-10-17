c8235e319ee86cb5d1607578c41337400898aec8
Ariel Batoon <info.arielbatoon@gmail.com>
2024-10-17 21:01:00 +0800

    Updated User Table Migration

--- app.backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ app.backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,8 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('user_role');
-            $table->string('flag');
+            $table->string('user_role')->default('user');
+            $table->string('flag')->default(1);
             $table->rememberToken();
             $table->timestamps();
         });
